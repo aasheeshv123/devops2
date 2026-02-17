52e5926c831481a371627b30be50f16029ab0daa
aasheesh vishwakarma <aasheeshv123@gmail.com>
2026-02-17 21:39:59 +0530

    ip changed

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  address_space       = ["10.55.77.99/16"]
+  address_space       = ["10.55.64.88/16"]
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
